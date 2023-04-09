ad531ec703a551f4f98e4ab4baa96be0c7f33d35
myrajarenga <myrajarenga1234@gmail.com>
2023-04-09 15:37:53 -0700

    creating folders to deploy

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,75 @@
+# Install Nginx
+class { 'nginx': }
+
+# Create necessary directories
+file { '/data/':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/releases/':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/shared/':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/releases/test/':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure => 'file',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0644',
+  content => '<html><body>Hello World</body></html>',
+}
+
+# Create symbolic link
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test/',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  require => File['/data/web_static/releases/test/'],
+}
+
+# Update Nginx configuration
+file { '/etc/nginx/sites-available/default':
+  content => "server {
+                listen 80;
+                listen [::]:80;
+                location /hbnb_static/ {
+                    alias /data/web_static/current/;
+                }
+            }",
+  notify => Service['nginx'],
+}
+
+# Set ownership of /data/ folder recursively
+exec { 'set_ownership':
+  command => 'chown -R ubuntu:ubuntu /data/',
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  onlyif  => 'test "$(stat -c %U:%G /data/)" != "ubuntu:ubuntu"',
+}
+
