7b51e02755769757d4eba46a37db41f0c53f65db
myrajarenga <myrajarenga1234@gmail.com>
2023-04-23 12:32:27 -0700

    script that starts flask web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,10 +2,13 @@
 """script to display HBNB"""
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def index():
     "function to return hello HBNB"
     return 'Hello HBNB'
+
 
 if "__name == __main__":
     app.run(host="0.0.0.0", port=5000)
