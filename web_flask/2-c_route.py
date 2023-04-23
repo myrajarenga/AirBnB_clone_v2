d2ecf5666a7fea225b402ed5639565d14152d8ab
myrajarenga <myrajarenga1234@gmail.com>
2023-04-23 13:02:40 -0700

    script that starts a Flask web application:

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,18 +2,25 @@
 """script to display HBNB"""
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def index():
     "function to return hello HBNB"
     return 'Hello HBNB'
+
+
 @app.route("/hbnb", strict_slashes=False)
 def index_hbnb():
     """ function to return HBNB"""
     return 'HBNB'
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def index_c(text):
     """function to return C is fun"""
-    return f'C:{text.replace("_", " ")}'
+    return 'C ' + text.replace("_", " ")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
